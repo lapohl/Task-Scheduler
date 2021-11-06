088645ac71ad798437cfe32c98f03fb282bcf17c
Lyndsey Pohl <lapohl@umich.edu>
2021-11-05 20:14:02 -0400

    deploy heroku

--- Golf_Task_Scheduler/golf_todo/golf_todo/settings.py
+++ Golf_Task_Scheduler/golf_todo/golf_todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'golf_todo.urls'
@@ -141,3 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
